cb068a2d783cc901e29835d10e4a8b8be440615c
[SL1ME322] <bioironman919@gmail.com>
2024-04-16 23:09:22 +0300

    q

---
+++ src/main/java/com/example/hostingtest/HostingTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.hostingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostingTestApplication.class, args);
+    }
+
+}
